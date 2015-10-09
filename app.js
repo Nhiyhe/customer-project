c37dba51b52f1b3143c518aaabb4ca96d8a3621d
Nhiyhe Adex <nhiyhe@gmail.com>
2015-10-09 22:36:38 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+ var express = require('express');
+ 
+ var app = express();
+ 
+ var port = process.env.PORT || 3000;
+ var customers = [{id:1,name:'James', lastname:'Scott'},{id:2,name:'Rob',lastname:'Hunter'},{id:3,name:'Peter',lastname:'Simon'}];
+ 
+ app.get('/customers',function(req,res){
+	res.json(customers); 
+ });
+ 
+ app.get('/customers/:id',function(req,res){
+	 var matchedItem;
+	var passedId = parseInt(req.params.id,10);
+	customers.forEach(function(item){
+		if(passedId == item.id){
+			matchedItem = item;
+		}
+	});
+	if(!matchedItem){
+		return res.status(404).send();
+	}else{
+		res.json(matchedItem);
+	}
+	
+ });
+ 
+ 
+ app.listen(port,function(){
+	 console.log('Server is running on port ' + port);
+ });

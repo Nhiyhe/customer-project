addcdc35ada5192cedab9665b0f78b69f90cb8ad
Nhiyhe Adex <nhiyhe@gmail.com>
2015-10-15 16:40:13 +0100

    jwt Token added

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcryptjs');
 var _ = require('underscore');
+var crypto = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, dataTypes){
 	var user = sequelize.define('users',{
@@ -68,6 +70,20 @@ module.exports = function(sequelize, dataTypes){
 			toPublicJSON:function(){
 				var json = this.toJSON();
 				return _.pick(json,'email','updatedAt','createdAt');
+			},
+			generateToken:function(type){
+				if(!_.isString(type)){
+					return undefined;
+				}
+				try{
+					var stringData = JSON.stringify({id:this.get('id'), type:type});
+					var encryptedData = crypto.AES.encrypt(stringData,'pass12£$').toString();
+					var token = jwt.sign({token:encryptedData},'jwt123@');
+					return token;
+				}catch(e){
+					return undefined;
+				}
+				
 			}
 		}
 	}
